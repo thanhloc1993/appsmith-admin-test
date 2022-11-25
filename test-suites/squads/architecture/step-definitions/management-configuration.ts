b7b57f027cf227d34896decccc200e246bdb0b0d
thanhloc1993 <loc.nguyen@manabie.com>
2022-11-25 13:55:28 +0700

    update url

--- test-suites/squads/architecture/step-definitions/management-configuration.ts
+++ test-suites/squads/architecture/step-definitions/management-configuration.ts
@@ -2,9 +2,9 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
 
-  await page.goto('http://localhost:3001/architecture/configuration');
+  await page.goto('https://backoffice.staging.manabie.io/lesson/lesson_management');
 
-  await page.goto('http://localhost:3001/login-tenant?redirectUrl=%2Farchitecture%2Fconfiguration');
+  await page.goto('https://backoffice.staging.manabie.io/login-tenant?redirectUrl=%2Flesson%2Flesson_management');
 
   await page.getByTestId('LoginTenantForm__textFieldOrganizations').click();
 
@@ -19,22 +19,8 @@ test('test', async ({ page }) => {
   await page.getByTestId('LoginTenantForm__textFieldPassword').fill('123123');
 
   await page.getByTestId('LoginTenantForm__buttonLogin').click();
-  await expect(page).toHaveURL('http://localhost:3001/architecture/configuration');
+  await expect(page).toHaveURL('https://backoffice.staging.manabie.io/lesson/lesson_management');
 
-  await page.frameLocator('internal:attr=[data-testid="Iframe_Appsmith_config-setting"]').locator('.sc-eaHRVx').first().click();
-
-  await page.frameLocator('internal:attr=[data-testid="Iframe_Appsmith_config-setting"]').getByTestId('container-wrapper-ilfrrxnd9s').locator('input[type="text"]').first().click();
-
-  await page.frameLocator('internal:attr=[data-testid="Iframe_Appsmith_config-setting"]').getByTestId('container-wrapper-ilfrrxnd9s').locator('input[type="text"]').first().fill('3');
-
-  await page.frameLocator('internal:attr=[data-testid="Iframe_Appsmith_config-setting"]').getByTestId('container-wrapper-ilfrrxnd9s').locator('input[type="text"]').nth(1).click();
-
-  await page.frameLocator('internal:attr=[data-testid="Iframe_Appsmith_config-setting"]').getByTestId('container-wrapper-ilfrrxnd9s').locator('input[type="text"]').nth(1).fill('3');
-
-  await page.frameLocator('internal:attr=[data-testid="Iframe_Appsmith_config-setting"]').getByTestId('container-wrapper-ilfrrxnd9s').locator('input[type="text"]').nth(2).click();
-
-  await page.frameLocator('internal:attr=[data-testid="Iframe_Appsmith_config-setting"]').getByTestId('container-wrapper-ilfrrxnd9s').locator('input[type="text"]').nth(2).fill('3');
-
-  await page.frameLocator('internal:attr=[data-testid="Iframe_Appsmith_config-setting"]').locator('#nfnjhq98vz div:has-text("Save")').nth(2).click();
+  await page.getByRole('button', { name: 'Filter' }).click();
 
 });
